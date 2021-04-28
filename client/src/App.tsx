6ea552fa413f9901a30d08dba8eb5aeb3f8aee89
Mariusz Izydorek <mariusz.izydorek@normanandsons.com>
2021-04-28 22:26:55 +0100

    login and SignUp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { HomePage } from "./pages/home/home";
+import Routes from "./pages/Routes";
 
 function App() {
-  return <HomePage />;
+  return <Routes />;
 }
 
 export default App;
